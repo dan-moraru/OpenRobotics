0b797e7c9b8dbcc4d3bb4006127b357a5e2a5238
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2026-02-22 21:38:12 -0500

    Scaffolded SimulationEngine class

--- open-robotics/src/main/java/com/openrobotics/simulationcore/SimulationEngine.java
+++ open-robotics/src/main/java/com/openrobotics/simulationcore/SimulationEngine.java
@@ -1,4 +1,41 @@
 package com.openrobotics.simulationcore;
 
 public class SimulationEngine {
+    private int tickCounter;
+    private boolean running; // tracks if the simulation is still running
+    private final CollisionManager collisionManager;
+
+    public SimulationEngine() {
+        tickCounter = 0;
+        running = false;
+        collisionManager = new CollisionManager();
+    }
+
+    // Runs a tick of the simulation
+    public void tick() {
+        // TODO
+    }
+
+    private void incrementTickCounter() {
+        tickCounter++;
+    }
+
+    // Collects move intentions for all robots in the simulation
+    private MoveIntention[] collectIntentions() {
+        // TODO
+
+        return null;
+    }
+
+    // Commits move intentions for all robots
+    private void CommitMoveIntentions() {
+        // TODO
+    }
+
+    // Updates states for all robots based on commited move intentions
+    private void updateRobotStates() {
+        // TODO
+    }
+
+
 }
