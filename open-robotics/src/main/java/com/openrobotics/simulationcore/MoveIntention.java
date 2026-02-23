3ad5060852417c860532662bf6364899914afa39
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2026-02-22 21:01:09 -0500

    created MoveIntention class

--- open-robotics/src/main/java/com/openrobotics/simulationcore/MoveIntention.java
+++ open-robotics/src/main/java/com/openrobotics/simulationcore/MoveIntention.java
@@ -1,4 +1,28 @@
 package com.openrobotics.simulationcore;
 
+import com.openrobotics.*;
+
+// Represents a move intention for a robot
 public class MoveIntention {
+    int robotId;
+    Tile from; // Current tile robot is intending to move away from
+    Tile to; // Tile robot is intended to move on
+
+    public MoveIntention(Tile from, Tile to, int robotId) {
+        this.from = from;
+        this.to = to;
+        this.robotId = robotId;
+    }
+
+    public int getRobotId() {
+        return robotId;
+    }
+
+    public Tile getFromTile() {
+        return from;
+    }
+
+    public Tile getToTile() {
+        return to;
+    }
 }
