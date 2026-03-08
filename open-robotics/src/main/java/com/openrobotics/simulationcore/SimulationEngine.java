b35737786fadafa18c39e413b126662477e1920b
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2026-03-08 16:21:42 -0400

    added getters for tick counter, isRunning, and robots from sim engine

--- open-robotics/src/main/java/com/openrobotics/simulationcore/SimulationEngine.java
+++ open-robotics/src/main/java/com/openrobotics/simulationcore/SimulationEngine.java
@@ -436,4 +436,17 @@ public class SimulationEngine {
     private void incrementTickCounter() {
         tickCounter++;
     }
+
+    // Getters
+    public int getTickCounter() {
+        return tickCounter;
+    }
+
+    public boolean getIsRunning() {
+        return running;
+    }
+
+    public Robot[] getRobots() {
+        return robots;
+    }
 }
