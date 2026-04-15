594844467d307db9c11929de996e06de9c3e648b
Filip Snitil <filip.snitil@mail.mcgill.ca>
2026-04-15 10:35:07 -0400

    javadoc: Obstacle, Rack, Station, ChargingStation, DeliveryStation

--- open-robotics/src/main/java/com/openrobotics/map/entities/environment/Obstacle.java
+++ open-robotics/src/main/java/com/openrobotics/map/entities/environment/Obstacle.java
@@ -5,7 +5,7 @@ import com.openrobotics.map.MapEntity;
 
 import java.util.UUID;
 
-/** impassable map entity; stops sensor rays and blocks traversal (uml 3.3.4) */
+/** Impassable map entity; stops sensor rays and blocks traversal (uml 3.3.4). */
 public class Obstacle extends MapEntity {
     public Obstacle(String name, Vector2D position) {
         super(name, position);
--- open-robotics/src/main/java/com/openrobotics/map/entities/station/DeliveryStation.java
+++ open-robotics/src/main/java/com/openrobotics/map/entities/station/DeliveryStation.java
@@ -4,7 +4,7 @@ import com.openrobotics.map.Vector2D;
 
 import java.util.UUID;
 
-/** delivery station; robots drop off boxes here to complete a task */
+/** Delivery station; robots drop off boxes here to complete a task. */
 public class DeliveryStation extends Station {
     public DeliveryStation(String name, Vector2D position) {
         super(name, position);
